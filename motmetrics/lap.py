6cb2a9fa3391b44ae22c9e1f2edab43a70fde736
Jack Valmadre <valmadre@google.com>
2019-11-29 15:02:30 +0100

    Make solvers more rigorous
    Add larger constant to replace nans which guarantees correct solution.
    This constant further guarantees correct solution with negative costs.
    Check that problem is feasible after solving with this constant.
    Add tests for negative costs, missing edges, non-integer edges.
    Clean up code for integer approximation.
    Add safety factor for integer approximation.
    Make tests parametric to expose which solver caused the error.

--- motmetrics/lap.py
+++ motmetrics/lap.py
@@ -1,5 +1,19 @@
 import numpy as np
 from collections import OrderedDict
+import logging
+import warnings
+
+
+# Largest magnitude integer that float32 can represent exactly.
+# https://stackoverflow.com/questions/3793838/
+# To see this:
+# >>> 2 ** 24 + 1
+# 16777217
+# >>> np.float32(2 ** 24 + 1)
+# 16777216.0
+# >>> np.float32(2 ** 24 - 1)
+# 16777215.0
+FLOAT32_MAX_INT = 2 ** 24 - 1
 
 def linear_sum_assignment(costs, solver=None):
     """Solve a linear sum assignment problem (LSA).
@@ -33,25 +47,38 @@ def linear_sum_assignment(costs, solver=None):
     cids = np.asarray(cids).astype(int)
     return rids, cids
 
+def _replace_nan_with_large_constant(costs):
+    # The linear_sum_assignment function in scipy does not support missing edges.
+    # Replace nan with a large constant that ensures it is not chosen.
+    # If it is chosen, that means the problem was infeasible.
+    valid = np.isfinite(costs)
+    if valid.all():
+        return costs
+    r = min(costs.shape)
+    # Assume all edges costs are within [-c, c], c >= 0.
+    # The cost of an invalid edge must be such that...
+    # choosing this edge once and the best-possible edge (r - 1) times
+    # is worse than choosing the worst-possible edge r times.
+    # l + (r - 1) (-c) > r c
+    # l > r c + (r - 1) c
+    # l > (2 r - 1) c
+    # Choose l = 2 r c + 1 > (2 r - 1) c.
+    c = np.abs(costs[valid]).max() + 1  # Doesn't hurt to add 1 here.
+    large_constant = 2 * r * c + 1
+    return np.where(valid, costs, large_constant)
+
 def lsa_solve_scipy(costs):
     """Solves the LSA problem using the scipy library."""
-
     from scipy.optimize import linear_sum_assignment as scipy_solve
-   
-    # Note there is an issue in scipy.optimize.linear_sum_assignment where
-    # it runs forever if an entire row/column is infinite or nan. We therefore
-    # make a copy of the distance matrix and compute a safe value that indicates
-    # 'cannot assign'. Also note + 1 is necessary in below inv-dist computation
-    # to make invdist bigger than max dist in case max dist is zero.
-    
-    inv = ~np.isfinite(costs)
-    if inv.any():
-        costs = costs.copy()
-        valid = costs[~inv]
-        INVDIST = 2 * valid.max() + 1 if valid.shape[0] > 0 else 1.
-        costs[inv] = INVDIST
 
-    return scipy_solve(costs)
+    finite_costs = _replace_nan_with_large_constant(costs)
+    rids, cids = scipy_solve(finite_costs)
+
+    # Ensure that no missing edges were chosen.
+    matching_costs = costs[rids, cids]
+    if not np.all(np.isfinite(matching_costs)):
+        raise ValueError('infeasible problem')
+    return rids, cids
 
 def lsa_solve_lapsolver(costs):
     """Solves the LSA problem using the lapsolver library."""
@@ -72,11 +99,22 @@ def lsa_solve_munkres(costs):
     indices = np.array(m.compute(costs), dtype=np.int64)
     return indices[:,0], indices[:,1]
 
+def _assert_integer(costs):
+    # Check that costs are not changed by rounding.
+    # Note: Elements of cost matrix may be nan.
+    np.testing.assert_equal(np.round(costs), costs)
+    # Require that costs are within representable range of integers for float32.
+    # This guarantees that int() will work.
+    # Note: Elements of cost matrix may be nan.
+    with warnings.catch_warnings():
+        warnings.simplefilter('ignore', RuntimeWarning)
+        within_range = np.logical_not(np.abs(costs) > FLOAT32_MAX_INT)
+    if not np.all(within_range):
+        raise AssertionError('costs are too large', np.max(np.abs(costs)))
 
 def lsa_solve_ortools(costs):
     """Solves the LSA problem using Google's optimization tools."""
     from ortools.graph import pywrapgraph
-
 
     # Google OR tools only support integer costs. Here's our attempt
     # to convert from floating point to integer:
@@ -92,60 +130,94 @@ def lsa_solve_ortools(costs):
     # For small min-diffs and large costs in general there is a change of
     # overflowing.
 
+    log10_scale = find_scale_for_integer_approximation(costs)
+    scale = 10 ** log10_scale
+
     valid = np.isfinite(costs)
-
-    min_e = -8
-    unique = np.unique(costs[valid])
-
-    if unique.shape[0] == 1:
-        min_diff = unique[0]
-    elif unique.shape[0] > 1:
-        min_diff = np.diff(unique).min()
-    else:
-        min_diff = 1
-
-    min_diff_e = 0
-    if min_diff != 0.0:
-        min_diff_e = int(np.log10(np.abs(min_diff)))
-        if min_diff_e < 0:
-            min_diff_e -= 1
-    
-    e = min(max(min_e, min_diff_e), 0)
-    f = 10**abs(e)
-
     assignment = pywrapgraph.LinearSumAssignment()
     for r in range(costs.shape[0]):
         for c in range(costs.shape[1]):
             if valid[r,c]:
-                assignment.AddArcWithCost(r, c, int(costs[r,c]*f))
-    
-    if assignment.Solve() != assignment.OPTIMAL:
-        return linear_sum_assignment(costs, solver='scipy')
+                assignment.AddArcWithCost(r, c, int(np.round(costs[r, c] * scale)))
 
+    status = assignment.Solve()
+    _ortools_assert_is_optimal(pywrapgraph, status)
+    return _ortools_extract_solution(assignment)
+
+def find_scale_for_integer_approximation(costs, max_log10_scale=8, log10_safety=2):
+    costs = np.asarray(costs)
+    costs = costs[np.isfinite(costs)]
+    if np.size(costs) == 0:
+        return 0
+
+    try:
+        _assert_integer(costs)
+    except AssertionError:
+        pass
+    else:
+        return 0
+    logging.warning('costs are not integers; using approximation')
+
+    # Find scale = 10 ** e such that:
+    # 1 / scale <= tol, or
+    # e = log(scale) >= -log tol
+    # where tol = min(diff(unique(costs)))
+    unique = np.unique(costs)
+    if np.size(unique) == 1:
+        # The magnitude of the cost does not matter at all.
+        return 0
+    min_diff = np.diff(unique).min()
+    e = int(np.ceil(np.log10(min_diff)))
+    # Add optional non-negative safety factor to reduce quantization noise.
+    e += max(log10_safety, 0)
+    # Ensure that we do not reduce the magnitude of the costs.
+    e = max(e, 0)
+    # Ensure that the scale is not too large.
+    if e > max_log10_scale:
+        logging.warning('could not achieve desired resolution for integer approximation: '
+                        'want 10 ** %d but max is 10 ** %d', e, max_log10_scale)
+        e = max_log10_scale
+    # TODO(valmadre): Check that costs * 10 ** e does not cause overflow.
+    return e
+
+def _ortools_assert_is_optimal(pywrapgraph, status):
+    if status == pywrapgraph.LinearSumAssignment.OPTIMAL:
+        pass
+    elif status == pywrapgraph.LinearSumAssignment.INFEASIBLE:
+        raise ValueError('infeasible assignment problem')
+    elif status == pywrapgraph.LinearSumAssignment.POSSIBLE_OVERFLOW:
+        raise ValueError('possible overflow in assignment problem')
+    else:
+        raise ValueError('unknown status')
+
+def _ortools_extract_solution(assignment):
     if assignment.NumNodes() == 0:
         return np.array([], dtype=np.int64), np.array([], dtype=np.int64)
 
     pairings = []
-    for i in range(assignment.NumNodes()):        
+    for i in range(assignment.NumNodes()):
         pairings.append([i, assignment.RightMate(i)])
 
     indices = np.array(pairings, dtype=np.int64)
-    return indices[:,0], indices[:,1]
+    return indices[:, 0], indices[:, 1]
+
+def _assert_solution_is_valid(costs, rids, cids):
+    matching_costs = costs[rids, cids]
+    if not np.all(np.isfinite(matching_costs)):
+        raise AssertionError('infeasible problem')
 
 def lsa_solve_lapjv(costs):
     from lap import lapjv
 
-    inv = ~np.isfinite(costs)
-    if inv.any():    
-        costs = costs.copy()    
-        valid = costs[~inv]
-        INVDIST = 2 * valid.max() + 1 if valid.shape[0] > 0 else 1.
-        costs[inv] = INVDIST
-
-    r = lapjv(costs, return_cost=False, extend_cost=True)
-    indices = np.array((range(costs.shape[0]), r[0]), dtype=np.int64).T        
+    finite_costs = _replace_nan_with_large_constant(costs)
+    row_to_col, _ = lapjv(finite_costs, return_cost=False, extend_cost=True)
+    indices = np.array((range(costs.shape[0]), row_to_col), dtype=np.int64).T
+    # Exclude unmatched rows (in case of unbalanced problem).
     indices = indices[indices[:, 1] != -1]
-    return indices[:,0], indices[:,1]
+    rids, cids = indices[:,0], indices[:,1]
+    # Ensure that no missing edges were chosen.
+    _assert_solution_is_valid(costs, rids, cids)
+    return rids, cids
 
 def init_standard_solvers():
     import importlib
@@ -201,5 +273,4 @@ def set_default_solver(newsolver):
         yield
     finally:
         default_solver = oldsolver
-    
 
--- motmetrics/tests/test_lap.py
+++ motmetrics/tests/test_lap.py
@@ -1,35 +1,75 @@
-from pytest import approx
+import pytest
 import numpy as np
 import motmetrics.lap as lap
 
+# Note: lapsolver solves MWM (Minimum-Weight Matching) not assignment.
+SOLVERS = ['ortools', 'scipy', 'munkres', 'lap']  # 'lapsolver'
 
-def test_lap_solvers():
-    assert len(lap.available_solvers) > 0
-    print(lap.available_solvers)
-
-    costs = np.array([[6, 9, 1],[10, 3, 2],[8, 7, 4.]])
+@pytest.mark.parametrize('solver', SOLVERS)
+def test_lap_full(solver):
+    costs = np.array([[6, 9, 1], [10, 3, 2], [8, 7, 4.]])
     costs_copy = costs.copy()
-    results = [lap.linear_sum_assignment(costs, solver=s) for s in lap.available_solvers]
+    result = lap.linear_sum_assignment(costs, solver=solver)
+
     expected = np.array([[0, 1, 2], [2, 1, 0]])
-    [np.testing.assert_allclose(r, expected) for r in results]
+    np.testing.assert_allclose(result, expected)
     np.testing.assert_allclose(costs, costs_copy)
 
-
-    costs = np.array([[5, 9, np.nan],[10, np.nan, 2],[8, 7, 4.]])
+@pytest.mark.parametrize('solver', SOLVERS)
+def test_lap_missing_edge(solver):
+    costs = np.array([[5, 9, np.nan], [10, np.nan, 2], [8, 7, 4.]])
     costs_copy = costs.copy()
-    results = [lap.linear_sum_assignment(costs, solver=s) for s in lap.available_solvers]
+    result = lap.linear_sum_assignment(costs, solver=solver)
+
     expected = np.array([[0, 1, 2], [0, 2, 1]])
-    [np.testing.assert_allclose(r, expected) for r in results]
+    np.testing.assert_allclose(result, expected)
+    np.testing.assert_allclose(costs, costs_copy)
+
+@pytest.mark.parametrize('solver', SOLVERS)
+def test_lap_non_integer(solver):
+    costs = (1. / 9) * np.array([[5, 9, np.nan], [10, np.nan, 2], [8, 7, 4.]])
+    costs_copy = costs.copy()
+    result = lap.linear_sum_assignment(costs, solver=solver)
+
+    expected = np.array([[0, 1, 2], [0, 2, 1]])
+    np.testing.assert_allclose(result, expected)
+    np.testing.assert_allclose(costs, costs_copy)
+
+@pytest.mark.parametrize('solver', SOLVERS)
+def test_lap_missing_edge_negative(solver):
+    """Test that missing edge is not used in assignment."""
+    costs = np.array([[-10000, -1], [-1, np.nan]])
+    costs_copy = costs.copy()
+    result = lap.linear_sum_assignment(costs, solver=solver)
+
+    # The optimal solution is (0, 1), (1, 0) for a cost of -2.
+    # Ensure that the algorithm does not choose the (0, 0) edge.
+    # This would not be a perfect matching.
+    expected = np.array([[0, 1], [1, 0]])
+    np.testing.assert_allclose(result, expected)
+    np.testing.assert_allclose(costs, costs_copy)
+
+@pytest.mark.parametrize('solver', SOLVERS)
+def test_lap_missing_edge_positive(solver):
+    """Test that missing edge is not used in assignment."""
+    costs = np.array([[np.nan, 1000, np.nan], [np.nan, 1, 1000], [1000, np.nan, 1]])
+    costs_copy = costs.copy()
+    result = lap.linear_sum_assignment(costs, solver=solver)
+
+    # The optimal solution is (0, 1), (1, 2), (2, 0) with cost 1000 + 1000 + 1000.
+    # A bad implementation may choose (0, 0), (1, 1), (2, 2) with cost inf + 1 + 1.
+    expected = np.array([[0, 1, 2], [1, 2, 0]])
+    np.testing.assert_allclose(result, expected)
     np.testing.assert_allclose(costs, costs_copy)
 
 def test_change_solver():
-    
+
     def mysolver(x):
         mysolver.called += 1
         return np.array([]), np.array([])
     mysolver.called = 0
 
-    costs = np.array([[6, 9, 1],[10, 3, 2],[8, 7, 4.]])
+    costs = np.array([[6, 9, 1], [10, 3, 2], [8, 7, 4.]])
 
     with lap.set_default_solver(mysolver):
         rids, cids = lap.linear_sum_assignment(costs)
